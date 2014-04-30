b6c048dfe9b8b3feaf38e1cb5b06ee9d64c7938d
Andrew Wittrock <awittrock@gmail.com>
2014-04-29 23:24:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dialup::Application.config.secret_key_base = '639fb22b9e70d6295e0594f2b257dd57b14a72e7e00390ba7a3907b75c48811872bdc780c9434d7c1a8de723d92922a59b29357d0d313c923dd80f6e734475d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dialup::Application.config.session_store :cookie_store, key: '_dialup_session'
